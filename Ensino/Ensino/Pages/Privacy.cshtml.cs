f4d59bc4ae57f08fe74cb1a72e0432e2a2756e95
SenhorYoh <yohanenrique8@gmail.com>
2026-04-08 09:58:46 +0100

    inicio

---
+++ Ensino/Ensino/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ensino.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
